d77a929b1bdd35cdd7e557806ab5aa7949fd04f4
Rishabh Shukla <68501064+RishabhShukla309@users.noreply.github.com>
2023-05-27 01:16:31 +0530

    commit

--- src/components/ReadingList/ReadingList.js
+++ src/components/ReadingList/ReadingList.js
@@ -6,7 +6,7 @@ const ReadingList = () => {
   return (<div className={`flex items-center mt-4`}>
     <Image src={avatarImage} alt={`image`} className={`rounded-md h-32 w-full`}/>
     <div className={`ml-2`}>
-      <h1 className={`text-sm font-bold text-slate-700`}>A beginnar's Guide to White Balance in photography.</h1>
+      <h1 className={`text-sm font-bold text-slate-700`}>A beginner Guide to White Balance in photography.</h1>
       <p className={`text-xs my-2 text-slate-400`}>Along with shutter speed,aperture an ISO, white balance is one of the</p>
       <div className={`flex justify-start items-center`}>
         <Image src={avatarImage} alt={`logo`} className={`inline-block h-6 w-6 rounded-full ring-2 ring-white`}/>
